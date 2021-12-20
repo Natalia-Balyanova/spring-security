33c2b6967a5b689d2014609b217327b1e5d37832
Natalia-Balyanova <75272081+Natalia-Balyanova@users.noreply.github.com>
2021-12-20 10:26:04 +0300

    Initial commit

---
+++ src/test/java/com/gb/balyanova/springsecurity/SpringSecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gb.balyanova.springsecurity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecurityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
